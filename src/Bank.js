99d1b58f36e6007c73fd34e5769db2b3533925c8
Martynas Skaringa <skarware@gmail.com>
2020-06-03 01:22:55 +0300

    add Bank class methods and stub methods for proccessing transactions

--- src/Bank.js
+++ src/Bank.js
@@ -1,5 +1,93 @@
 export default class Bank {
+
+    // create separate module for commission fees configuration?
+    static commissionFeesConfigURLs = {
+        cashIn: "http://private-anon-e3a33b01c6-uzduotis.apiary-mock.com/config/cash-in;",
+        cashOutNatural: "http://private-anon-e3a33b01c6-uzduotis.apiary-mock.com/config/cash-out/natural",
+        cashOutJuridical: "http://private-anon-e3a33b01c6-uzduotis.apiary-mock.com/config/cash-out/juridical"
+    };
+
+    static getFeesConfig = function getCommissionFeesConfigurationFromAPI() {
+
+        // invoke on object instantiation with constructor or static bock?
+
+    }
+
+    static commissionFeesConfigs = {
+        cashIn: {
+            "percents": 0.03,
+            "max": {
+                "amount": 5,
+                "currency": "EUR"
+            }
+        },
+        cashOutNatural: {
+            "percents": 0.3,
+            "week_limit": {
+                "amount": 1000,
+                "currency": "EUR"
+            }
+        },
+        cashOutJuridical: {
+            "percents": 0.3,
+            "min": {
+                "amount": 0.5,
+                "currency": "EUR"
+            }
+        }
+    };
+
+    // same log for all branches of the bank
+    static transactionsLog = [];
+
     constructor(name) {
         this.name = name;
+    }
+
+    processTransaction(transaction) {
+        console.info('processTransaction(transaction): ', transaction); ////// FOR DEVELOPING PURPOSES ONLY
+
+        // get commission fee for given transaction
+        const commissionFee = this.getCommissionFee(transaction);
+
+        // print commission fee to stdout
+        console.log(commissionFee)
+
+        // after we done processing transaction add it to the log for the future reference
+        this.logTransaction(transaction);
+    }
+
+    logTransaction(transaction) {
+        Bank.transactionsLog.push(transaction)
+    }
+
+
+    getCommissionFee(transaction) {
+        // initial commissionFee
+        let commissionFee = 0;
+
+        // First get the right commission fee config for given transaction
+        const feeConfig = this.getFeeConfig(transaction);
+
+
+        return undefined;
+    }
+
+    calcCommissionFee(transaction) {
+
+
+        return undefined;
+    }
+
+    getFeeConfig({type, userType}) {
+        // fee types 'cashIn','cashOutNatural','cashOutJuridical'
+        let feeConfig = Bank.commissionFeesConfigs.cashIn; // default
+        if (type === 'cashOut') {
+            feeConfig = Bank.commissionFeesConfigs.cashOutNatural
+            if (userType === 'juridical') {
+                feeConfig = Bank.commissionFeesConfigs.cashOutJuridical;
+            }
+        }
+        return feeConfig;
     }
 }
--- src/app.js
+++ src/app.js
@@ -1,7 +1,36 @@
 import Bank from './Bank.js';
 
-let bank = new Bank('Bankas');
+const camelCase = function snakeToCamelCase(json) {
+    return json.replace(/([-_][a-z])/g, (capturedGroup) => capturedGroup[1].toUpperCase());
+}
 
-console.log('hello world app.js')
+const toDateObj = function stringToDateObj(key, value) {
+    if (key === 'date') return new Date(value);
+    return value;
+}
 
-console.log(bank.name)
+const operationsArr = [ ////// FOR DEVELOPING PURPOSES ONLY
+    { "date": "2016-01-05", "user_id": 1, "user_type": "natural", "type": "cash_in", "operation": { "amount": 200.00, "currency": "EUR" } },
+    { "date": "2016-01-06", "user_id": 2, "user_type": "juridical", "type": "cash_out", "operation": { "amount": 300.00, "currency": "EUR" } },
+    { "date": "2016-01-06", "user_id": 1, "user_type": "natural", "type": "cash_out", "operation": { "amount": 30000, "currency": "EUR" } },
+    { "date": "2016-01-07", "user_id": 1, "user_type": "natural", "type": "cash_out", "operation": { "amount": 1000.00, "currency": "EUR" } },
+    { "date": "2016-01-07", "user_id": 1, "user_type": "natural", "type": "cash_out", "operation": { "amount": 100.00, "currency": "EUR" } },
+    { "date": "2016-01-10", "user_id": 1, "user_type": "natural", "type": "cash_out", "operation": { "amount": 100.00, "currency": "EUR" } },
+    { "date": "2016-01-10", "user_id": 2, "user_type": "juridical", "type": "cash_in", "operation": { "amount": 1000000.00, "currency": "EUR" } },
+    { "date": "2016-01-10", "user_id": 3, "user_type": "natural", "type": "cash_out", "operation": { "amount": 1000.00, "currency": "EUR" } },
+    { "date": "2016-02-15", "user_id": 1, "user_type": "natural", "type": "cash_out", "operation": { "amount": 300.00, "currency": "EUR" } },
+] ////// FOR DEVELOPING PURPOSES ONLY
+
+let bankVilniusBranch = new Bank('Vilniaus filialas');
+
+// const operation = JSON.parse(camelCase(JSON.stringify(operationsArr[0])), toDateObj); ////// FOR DEVELOPING PURPOSES ONLY
+// bank.processTransaction(operation); ////// FOR DEVELOPING PURPOSES ONLY
+
+for (let i = 0; operationsArr.length > i ; i++) { ////// FOR DEVELOPING PURPOSES ONLY
+    bankVilniusBranch.processTransaction(JSON.parse(camelCase(JSON.stringify(operationsArr[i])), toDateObj)); ////// FOR DEVELOPING PURPOSES ONLY
+} ////// FOR DEVELOPING PURPOSES ONLY
+
+// bank.transactionsLog.forEach(v => console.log('transactionLog: ', {v})); ////// FOR DEVELOPING PURPOSES ONLY
+
+
+
