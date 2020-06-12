700edd5d1f92be1f0458b7f20f3b02f7c7e28d46
Martynas Skaringa <skarware@gmail.com>
2020-06-12 13:47:20 +0300

    Refactor Utils, add removeTrailingComma method

--- src/Utils.js
+++ src/Utils.js
@@ -1,16 +1,32 @@
 export default class Utils {
-
+    /**
+     *  Converts snake_case to camelCase in given JSON (string);
+     */
     static camelCase = function snakeToCamelCase(json) {
         return json.replace(/([-_][a-z])/g, (capturedGroup) => capturedGroup[1].toUpperCase());
     }
-
+    /**
+     *  Transforms date string format to Date object
+     */
     static toDateObj = function stringToDateObj(key, value) {
         if (key === 'date') return new Date(value);
         return value;
     }
-
+    /**
+     *  Capitalizes first letter in given word (string).
+     */
     static capitalize = function capitalizeFirstLetter(str) {
         return str.charAt(0).toUpperCase() + str.slice(1);
     }
-
+    /**
+     * Removes any trailing commas in given JSON (string);
+     * Find ,, after which there is no any new attribute, object or array.
+     * New attribute could start either with quotes (" or ') or with any word-character (\w).
+     * New object could start only with character {.
+     * New array could start only with character [.
+     * New attribute, object or array could be placed after a bunch of space-like symbols (\s).
+     */
+    static removeTrailingComma = function removeTrailingCommaFromJSON(json) {
+        return json.replace(/\,(?!\s*?[\{\[\"\'\w])/g, '');
+    }
 }
