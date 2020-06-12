85e163b4cf708ab8c430f6b5f6a0ed4ea62499c9
Martynas Skaringa <skarware@gmail.com>
2020-06-12 16:27:15 +0300

    Improve commissionFees singelton module design

--- src/commissionFees.js
+++ src/commissionFees.js
@@ -1,5 +1,10 @@
+import Utils from './Utils.js';
+
 export default (function commissionFeesSingletonFactory() {
-    // export commissions fees module as singleton, not that the current app version need it but more like to show off or try singleton implementation using lazy initialization pattern in JS
+    /**
+     * Export commissions fees module as singleton, not that the current app version need it but
+     * more like to show off or try singleton implementation using lazy initialization pattern in JS
+     */
     function commissionFees() {
 
         // Object with all currently know commission fees configs
@@ -13,19 +18,26 @@ export default (function commissionFeesSingletonFactory() {
 
             // maybe should implement plural version, faster async version with Promise.all()
 
+            // Utils.camelCase(JSON.received.from.API);
+
+
             // return = await Promise.all(feeConfigs);
 
         }
 
-        // Empty commissions fees config object which will be filled by getFeesConfigObject
-        const feesConfig = {}; // const so that referenced obj do not change and updateFeesConfig method would update existing object
+        /**
+         * Empty commissions fees config object which will be filled by getFeesConfig;
+         * const so that referenced obj do not change and updateFeesConfig method would update existing object correctly.
+         */
+        const feesConfig = {};
 
-        const getFeesConfig = async function getCommissionFeesConfigurationFromAPI() {
+        // const getFeesConfig = async function getCommissionFeesConfigurationFromAPI() {
+        const getFeesConfig = function getCommissionFeesConfigurationFromAPI() {
 
             // In case invoked by updateFeesConfig method first deep clean up of existing properties of feesConfig object
-            Object.keys(feesConfig).forEach((key) => {
-                delete feesConfig[key]
-            });
+            // Object.keys(feesConfig).forEach((key) => {
+            //     delete feesConfig[key]
+            // });
 
             // Populate feeConfigs object with commission fees configs received from API with same names as found in configUrls parameter
             // for (const [key, value] of Object.entries(configUrls)) {
@@ -38,10 +50,11 @@ export default (function commissionFeesSingletonFactory() {
             //     feesConfig[key] = fetchFeesConfig(value);
             // });
 
-            console.log(feesConfig);    ////// FOR DEVELOPING PURPOSES ONLY
-            //
-            // Do not return anything here as method will directly change feesConfig variable
-            //
+             console.log(feesConfig);    ////// FOR DEVELOPING PURPOSES ONLY
+
+            /**
+             * Do not return anything here as method will directly change feesConfig variable
+             */
         }
 
         // Updates a single instance object of commission fees configuration just in case those changes
@@ -50,8 +63,9 @@ export default (function commissionFeesSingletonFactory() {
             getFeesConfig();
         }
 
-        // Make a closure to separate public and private module members
+        // Return public interface by making a closure to separate public and private module members.
         return {
+            // private members are accessible here but not on returned obj (public interface)
             updateFeesConfig: updateFeesConfig,
             getFeesConfig: feesConfig,
         }
@@ -61,9 +75,17 @@ export default (function commissionFeesSingletonFactory() {
     return {
         // Singleton's Gatekeeper - returns the one and only instance of the commissionFees object
         getInstance: () => {
-            if (!instance) {    // check if instance is available
+            // check if instance is available if not then and only then create it
+            if (!instance) {
                 instance = new commissionFees();
-                delete instance.constructor; // or set it to null
+
+                // fetch on initialization fees configuration from API
+                instance.updateFeesConfig();
+                /**
+                 * null the constructor so the returned object can't be new'd.
+                 * NOTE: that delete instance.constructor; won't do the job!
+                 */
+                instance.constructor = null;
             }
             return instance;
         }
